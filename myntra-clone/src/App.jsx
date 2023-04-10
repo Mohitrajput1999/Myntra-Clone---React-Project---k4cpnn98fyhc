c51b31260106f15808db357b526b3b692474fdaa
Mohit <r.mohit0199@gmail.com>
2023-04-10 10:53:01 +0530

    Added

---
+++ myntra-clone/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import {createBrowserRouter} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {path: '/',
+   element: <h1>The</h1> 
+  }])
+function App() {
+  return  <h1>Hii</h1>
+}
+
+export default App
