28afa0c52ca6c0dacce3ddd52fe2504d035bfa84
Mohit <r.mohit0199@gmail.com>
2023-04-20 14:30:56 +0530

    Done

--- src/Pages/CartPages/PaymentPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PaymentPro from "../../Components/Payment/PaymentPro"
-export const PaymentPage = () => {
-  return (
-    <>
-      <PaymentPro />
-    </>
-  );
-};
